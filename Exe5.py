eab1a969e64b4463e558241c2c3cc318e4de58db
rogercf17 <rogercf16@gmail.com>
2024-03-09 12:56:07 -0300

    Enviando exercício 5

---
+++ Exe5.py
@@ -0,0 +1,19 @@
+v1 = int(input('Digite o 1° valor: '))
+v2 = int(input('Digite o 2° valor: '))
+v3 = int(input('Digite o 3° valor: '))
+
+if v1 == v2 or v1 == v3 or v2 == v3:
+    print('Alguns dos valores são iguais. Digite números diferentes!')
+else:
+    if v1 > v2 > v3:
+        print(f'A ordem crescente é: {v3}-{v2}-{v1}')
+    elif v1 > v3 > v2:
+        print(f'A ordem crescente é: {v2}-{v3}-{v1}')
+    elif v2 > v1 > v3:
+        print(f'A ordem crescente é: {v3}-{v1}-{v2}')
+    elif v2 > v3 > v1:
+        print(f'A ordem crescente é: {v1}-{v3}-{v2}')
+    elif v3 > v1 > v2:
+        print(f'A ordem crescente é: {v2}-{v1}-{v3}')
+    elif v3 > v2 > v1:
+        print(f'A ordem crescente é: {v1}-{v2}-{v3}')
