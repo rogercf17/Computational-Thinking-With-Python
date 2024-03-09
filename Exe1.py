c1b212e8003abd3783ca2065790342d807e6f779
rogercf17 <rogercf16@gmail.com>
2024-03-09 13:00:12 -0300

    Exercício 1 modificado

--- Exe1.py
+++ Exe1.py
@@ -1,7 +1,7 @@
-v1 = int(input('Digite o 1° número: '))
+v = int(input('Digite o 1° número: '))
 v2 = int(input('Digite o 2° número: '))
 
-if v1 > v2:
-    print(f'O maior número é {v1}')
+if v > v2:
+    print(f'O maior número é {v}')
 else:
     print(f'O maior número é {v2}')
