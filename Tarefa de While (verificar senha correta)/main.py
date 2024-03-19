0228da7a56180e3ad6bbbcfea5cab69ea64a6c94
rogercf17 <rogercf16@gmail.com>
2024-03-19 20:51:22 -0300

    print adicionado

--- Tarefa de While (verificar senha correta)/main.py
+++ Tarefa de While (verificar senha correta)/main.py
@@ -2,3 +2,4 @@ senha = input('Digite a senha para entrar: ')
 senha_correta = '1234'
 while senha != senha_correta:
     senha = input('Senha incorreta! Digite novamente: ')
+print('Acesso liberado')
